9b9cc10d2607d410385afc697715e8462f9a4352
Alexandr Meshkov <alex.meh96@mail.ru>
2024-03-08 00:00:04 +0300

    core_apps jvmti: added

---
+++ core_apps/src/main/java/tools/jvmti/Main.java
@@ -0,0 +1,7 @@
+//package tools.jvmti;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
---
+++ core_apps/src/main/java/tools/jvmti/agent.c
@@ -0,0 +1,21 @@
+#include <jvmti.h>
+#include <stdio.h>
+
+JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* vm, char* options, void* reserved) {
+    jvmtiEnv* jvmti;
+
+    // Get JVMTI environment
+    jint result = (*vm)->GetEnv(vm, (void**)&jvmti, JVMTI_VERSION_1);
+    if (result != JNI_OK || jvmti == NULL) {
+        printf("Unable to access JVMTI environment\n");
+        return JNI_ERR;
+    }
+
+    char* vm_name = NULL;
+    (*jvmti)->GetSystemProperty(jvmti, "java.vm.name", &vm_name);
+
+    printf("Agent loaded. JVM name = %s\n", vm_name);
+    fflush(stdout);
+
+    return 0;
+}
